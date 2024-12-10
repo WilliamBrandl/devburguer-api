2170961b7136d5c7e78d0df3e28d204c54268da8
williambrandl <william.brandl@icloud.com>
2024-12-09 21:41:35 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express, { json } from 'express';
+import routes from './routes';
+
+class App {
+
+    constructor() {
+        this.app = express();
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    middlewares() {
+
+        this.app.use(json());
+    }
+
+    routes() {
+        this.app.use(routes);
+    }
+
+}
+
+export default new App().app
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import { listen } from './app'
+
+listen(3001, () => console.log('Server is running at port 3001...') )
